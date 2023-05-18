07e9d1131bcfe1813f71ecf0b598729c9926400d
dinglezhang <polycplusplus@gmail.com>
2023-05-18 10:31:18 +0800

    add log to change log level in test.py

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import numpy as np
 
+from helpers import util
+
 from tests import test_util
 from tests import test_euler
 from tests import test_attitude
 from tests import test_rotate_vectors
+
+logger = util.get_logger()
+# log level is 'INFO' by default, set it as ''WARNING' if you want to close most of them
+#logger.setLevel('WARNING')
 
 if __name__ == '__main__':
   np.set_printoptions(precision = 8, suppress = True)
