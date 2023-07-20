ea66f3813ca7928159a30b1c0ddbe97c3b39bcde
dinglezhang <polycplusplus@gmail.com>
2023-07-20 17:56:57 +0800

    use variables to define the same test parameters

--- tests/test_attitude.py
+++ tests/test_attitude.py
@@ -10,29 +10,43 @@ from RotEx import attitude
 
 from . import test_rotate_vectors
 
-@pytest.mark.parametrize('rfu_d_in_enu_frame, expected_rfu_d_in_enu_frame',
-                        [(np.array([-45, 0, 0]), np.array([45, 0, 0])),
-                         (np.array([0, 45, 0]), np.array([0, 45, 0])),
-                         (np.array([0, 0, 45]), np.array([0, 0, 45])),
-                         (np.array([-90, 45, 90]), np.array([90, 45, 90]))])
-def test_change_frame_enu_2_ned(rfu_d_in_enu_frame, expected_rfu_d_in_enu_frame):
+att_in_frame_1 = \
+  [np.array([-45, 0, 0]),
+   np.array([0, 45, 0]),
+   np.array([0, 0, 45]),
+   np.array([-90, 45, 90])]
+
+att_in_frame_2 = \
+  [np.array([45, 0, 0]),
+   np.array([0, 45, 0]),
+   np.array([0, 0, 45]),
+   np.array([90, 45, 90])]
+
+heading_samples = \
+  [np.array([-1, 1, math.sqrt(2)]),
+   np.array([-1, 2, 3]),
+   np.array([-1, 2, 0.5])]
+
+slope_angle_samples = [0, 15, 45, -30]
+
+att_d_smaples = \
+  [np.array([10, 1, 4]),
+   np.array([11, 2, 5])]
+
+rot_seq_samples = ['ZYX', 'zyx']
+
+@pytest.mark.parametrize('rfu_d_in_enu_frame, rfu_d_in_enu_frame_expected', [(a, b) for a, b in zip(att_in_frame_1, att_in_frame_2)])
+def test_change_frame_enu_2_ned(rfu_d_in_enu_frame, rfu_d_in_enu_frame_expected):
   (rot_in_ned_frame, frd_d_in_ned_frame) = attitude.change_frame_enu_2_ned(rfu_d_in_enu_frame, True)
-  assert_allclose(frd_d_in_ned_frame, expected_rfu_d_in_enu_frame, atol = 1e-8)
+  assert_allclose(frd_d_in_ned_frame, rfu_d_in_enu_frame_expected, atol = 1e-8)
 
-@pytest.mark.parametrize('frd_d_in_ned_frame, expected_frd_d_in_ned_frame',
-                        [(np.array([45, 0, 0]), np.array([-45, 0, 0])),
-                         (np.array([0, 45, 0]), np.array([0, 45, 0])),
-                         (np.array([0, 0, 45]), np.array([0, 0, 45])),
-                         (np.array([90, 45, 90]), np.array([-90, 45, 90]))])
-def test_change_frame_ned_2_enu(frd_d_in_ned_frame, expected_frd_d_in_ned_frame):
+@pytest.mark.parametrize('frd_d_in_ned_frame, frd_d_in_ned_frame_expected', [(a, b) for a, b in zip(att_in_frame_2, att_in_frame_1)])
+def test_change_frame_ned_2_enu(frd_d_in_ned_frame, frd_d_in_ned_frame_expected):
   (rot_in_enu_frame, rfu_d_in_enu_frame) = attitude.change_frame_ned_2_enu(frd_d_in_ned_frame, True)
-  assert_allclose(rfu_d_in_enu_frame, expected_frd_d_in_ned_frame, atol = 1e-8)
+  assert_allclose(rfu_d_in_enu_frame, frd_d_in_ned_frame_expected, atol = 1e-8)
 
-@pytest.mark.parametrize('heading_as_rfu',
-                        [np.array([-1, 1, math.sqrt(2)]),
-                         np.array([-1, 2, 3]),
-                         np.array([-1, 2, 0.5])])
-@pytest.mark.parametrize('right_slope_angle_d', [0, 15, 45, -30])
+@pytest.mark.parametrize('heading_as_rfu', heading_samples)
+@pytest.mark.parametrize('right_slope_angle_d', slope_angle_samples)
 def test_from_heading_in_enu_frame(heading_as_rfu, right_slope_angle_d):
   is_possible = rotex._analyze_vector_and_angle_against_xy_plane(heading_as_rfu, np.deg2rad(right_slope_angle_d))[0]
 
@@ -55,11 +69,8 @@ def test_from_heading_in_enu_frame(heading_as_rfu, right_slope_angle_d):
 
     assert_allclose(right_slope_angle_result, right_slope_angle_d, atol = 1e-8)
 
-@pytest.mark.parametrize('heading_as_frd',
-                        [np.array([-1, 1, math.sqrt(2)]),
-                         np.array([-1, 2, 3]),
-                         np.array([-1, 2, 0.5])])
-@pytest.mark.parametrize('right_slope_angle_d', [0, 15, 45, -30])
+@pytest.mark.parametrize('heading_as_frd', heading_samples)
+@pytest.mark.parametrize('right_slope_angle_d', slope_angle_samples)
 def test_from_heading_in_ned_frame(heading_as_frd, right_slope_angle_d):
   is_possible = rotex._analyze_vector_and_angle_against_xy_plane(heading_as_frd, np.deg2rad(right_slope_angle_d))[0]
 
@@ -82,9 +93,9 @@ def test_from_heading_in_ned_frame(heading_as_frd, right_slope_angle_d):
 
     assert_allclose(right_slope_angle_result, right_slope_angle_d, atol = 1e-8)
 
-@pytest.mark.parametrize('att_d_1', [np.array([10, 1, 4])])
-@pytest.mark.parametrize('att_d_2', [np.array([11, 2, 5])])
-@pytest.mark.parametrize('rot_seq', ['ZYX', 'zyx'])
+@pytest.mark.parametrize('att_d_1', att_d_smaples)
+@pytest.mark.parametrize('att_d_2', att_d_smaples)
+@pytest.mark.parametrize('rot_seq', rot_seq_samples)
 @pytest.mark.parametrize('in_world_frame', [True, False])
 def test_get_delta_att(att_d_1, att_d_2, rot_seq, in_world_frame, vector_samples):
   rot1 = Rotation.from_euler(rot_seq, att_d_1, True)
@@ -101,11 +112,11 @@ def test_get_delta_att(att_d_1, att_d_2, rot_seq, in_world_frame, vector_samples
   (delta_rot, delta_euler_d) = attitude.get_delta_att(att_d_1, att_d_2, rot_seq, True, in_world_frame)
   vectors_by_delta_rot = delta_rot.apply(vectors_by_rot1)
 
-  assert_allclose(vectors_by_rot2, vectors_by_delta_rot)
+  assert_allclose(vectors_by_rot2, vectors_by_delta_rot, atol = 1e-8)
 
-@pytest.mark.parametrize('att_d_1', [np.array([10, 1, 4]), np.array([11, 2, 5])])
+@pytest.mark.parametrize('att_d_1', att_d_smaples)
 @pytest.mark.parametrize('factor', [1.1, 1.2])
-@pytest.mark.parametrize('rot_seq', ['ZYX', 'zyx'])
+@pytest.mark.parametrize('rot_seq', rot_seq_samples)
 def test_linear_delta_att(att_d_1, factor, rot_seq):
   # calculate att_d_2 from att_d_1 and factor (linear change on rotvec)
   rot1 = Rotation.from_euler(rot_seq, att_d_1, True)
@@ -124,9 +135,9 @@ def test_linear_delta_att(att_d_1, factor, rot_seq):
 
   assert_allclose(delta_euler_d, delta_euler_d_linear)
 
-@pytest.mark.parametrize('att_d_1', [np.array([10, 1, 4])])
-@pytest.mark.parametrize('att_d_2', [np.array([11, 2, 5])])
-@pytest.mark.parametrize('rot_seq', ['ZYX', 'zyx'])
+@pytest.mark.parametrize('att_d_1', att_d_smaples)
+@pytest.mark.parametrize('att_d_2', att_d_smaples)
+@pytest.mark.parametrize('rot_seq', rot_seq_samples)
 @pytest.mark.parametrize('delta_time', [2, 3])
 def test_calc_angular_velocity(att_d_1, att_d_2, rot_seq, delta_time):
   angular_velocity = attitude.calc_angular_velocity(att_d_1, att_d_2, rot_seq, True, delta_time, False)[0]
@@ -139,4 +150,4 @@ def test_calc_angular_velocity(att_d_1, att_d_2, rot_seq, delta_time):
   angular_velocity_spline = spline(times, 1)[1]
   angular_velocity_spline = np.rad2deg(angular_velocity_spline)
 
-  assert_allclose(angular_velocity, angular_velocity_spline)
+  assert_allclose(angular_velocity, angular_velocity_spline, atol = 1e-8)
--- tests/test_rotex.py
+++ tests/test_rotex.py
@@ -7,6 +7,18 @@ from RotEx import rotex
 
 from . import test_rotate_vectors
 
+rot_samples = \
+  [Rotation.from_euler('ZYX', np.array([1, 2, 3]), True),
+   Rotation.from_euler('ZYX', np.array([-1, 2, -3]), True),
+   Rotation.from_euler('ZYX', np.array([3, -2, 10]), True),
+   Rotation.from_euler('ZYX', np.array([-10, -1, 3]), True)]
+
+rot_in_small_angles_samples = \
+  [Rotation.from_euler('ZYX', np.array([0.1, 0.2, 0.3]), True),
+   Rotation.from_euler('ZYX', np.array([-0.1, 0.2, -0.3]), True),
+   Rotation.from_euler('ZYX', np.array([0.3, -0.2, 0.1]), True),
+   Rotation.from_euler('ZYX', np.array([-0.2, -0.1, 0.3]), True)]
+
 @pytest.mark.parametrize('v1', [np.array([0, 3, 0]), np.array([1, 2, 3])])
 @pytest.mark.parametrize('v2', [np.array([2, 3, 4]), np.array([4, 5, 6])])
 @pytest.mark.parametrize('self_roll_angle', [0, 15, 60, 90])
@@ -42,11 +54,7 @@ def test_get_rot_in_new_frame(rot_in_old_frame, rot_old_2_new_frame, vector_samp
 
   assert_allclose(vectors_rotated_in_new_frame, vectors_rotated_in_new_frame_expected)
 
-@pytest.mark.parametrize('rot',
-                        [Rotation.from_euler('ZYX', np.array([1, 2, 3]), True),
-                         Rotation.from_euler('ZYX', np.array([-1, 2, -3]), True),
-                         Rotation.from_euler('ZYX', np.array([3, -2, 10]), True),
-                         Rotation.from_euler('ZYX', np.array([-10, -1, 3]), True)])
+@pytest.mark.parametrize('rot', rot_samples)
 def test_calc_linear_displacement(rot, vector_samples):
   linear_displacement_scalars = rotex.calc_linear_displacement(rot, vector_samples)[1]
 
@@ -64,28 +72,20 @@ def test_calc_linear_displacement(rot, vector_samples):
 
   assert_allclose(linear_displacement_scalars, linear_displacement_scalars_expected, atol = 1e-2)
 
-@pytest.mark.parametrize('rot',
-                        [Rotation.from_euler('ZYX', np.array([0.1, 0.2, 0.3]), True),
-                         Rotation.from_euler('ZYX', np.array([-0.1, 0.2, -0.3]), True),
-                         Rotation.from_euler('ZYX', np.array([0.3, -0.2, 0.1]), True),
-                         Rotation.from_euler('ZYX', np.array([-0.2, -0.1, 0.3]), True)])
+@pytest.mark.parametrize('rot', rot_in_small_angles_samples)
 @pytest.mark.parametrize('delta_time', [1, 2, 3])
-def test_calc_linear_velocity_with_small_angle(rot, vector_samples, delta_time):
+def test_calc_linear_velocity_in_small_angles(rot, vector_samples, delta_time):
   linear_velocities = rotex.calc_linear_velocity(rot, vector_samples, delta_time)[0]
 
-  # calculate approx velocities by two vectors with small angle rotation
+  # calculate approx velocities by two vectors in small angles rotation
   vectors_rotated = rot.apply(vector_samples)
   linear_velocities_approx = (vectors_rotated - vector_samples) / delta_time
 
   assert_allclose(linear_velocities, linear_velocities_approx, atol = 1e-3)
 
-@pytest.mark.parametrize('rot',
-                        [Rotation.from_euler('ZYX', np.array([0.1, 0.2, 0.3]), True),
-                         Rotation.from_euler('ZYX', np.array([-0.1, 0.2, -0.3]), True),
-                         Rotation.from_euler('ZYX', np.array([0.3, -0.2, 0.1]), True),
-                         Rotation.from_euler('ZYX', np.array([-0.2, -0.1, 0.3]), True)])
+@pytest.mark.parametrize('rot', rot_in_small_angles_samples)
 @pytest.mark.parametrize('delta_time', [1, 2, 3])
-def test_calc_centripetal_acceleration_with_small_angle(rot, vector_samples, delta_time):
+def test_calc_centripetal_acceleration_in_small_angles(rot, vector_samples, delta_time):
   centripetal_acceleration_vectors = rotex.calc_centripetal_acceleration(rot, vector_samples, delta_time)[0]
   rot_axis = rot.as_rotvec()
 
@@ -93,7 +93,7 @@ def test_calc_centripetal_acceleration_with_small_angle(rot, vector_samples, del
   angles = np.arccos(np.dot(centripetal_acceleration_vectors, rot_axis) / (np.linalg.norm(rot_axis) * np.linalg.norm(centripetal_acceleration_vectors)))
   assert_allclose(angles, np.full(vector_samples.shape[0], np.deg2rad(90)), atol = 1e-8)
 
-  # calculate approx acceleration by two vectors with small angle rotation
+  # calculate approx acceleration by two vectors in small angles rotation
   vectors_rotated_before = rot.inv().apply(vector_samples)
   vectors_rotated_after = rot.apply(vector_samples)
   linear_velocities_before = (vector_samples - vectors_rotated_before) / delta_time
